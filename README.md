0afe2f46f8c25d42d8f8ca6e31e6607d19cd3667
Vinícius Godoy <vinigodoy@gmail.com>
2018-05-14 15:20:33 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Aula 05 - Iluminação: Componentes ambiente e difuso
+
+Neste repositório apresentamos o código intermediário da aula de iluminação contendo também o componente especular e os 
+materiais.
+
+## Componente especular
+
+Representa a luz que reflete no objeto diretamente sobre os olhos do observador. Isso faz com que um brilho intenso 
+apareça. Normalmente, a cor do reflexo especular será branco, a menos que o objeto seja um metal. A propriedade do 
+material specular power controla o quão disperso é o brilho.
+
+## Materiais
+
+Para cada componente da luz, permitimos especificar como o material absorve os valores de R, G e B. Assim como foi feito
+com a luz, o material também é aplicado através de uma operação de produto. Assim, por exemplo, se uma luz brilha com 
+os valores 0.8, 0.8, 0.4 em R, G e B e o material reflete 50% de R, 80% de verde e nenhum azul, o material será 
+especificado por 0.5, 0.8, 0.0. Portanto, a cor final será 0.8*0.5, 0.8*0.8, 0.4*0.0 = 0.4, 0.64, 0.0.
---
+++ src/br/pucpr/cg/LitCube.java
@@ -0,0 +1,85 @@
+package br.pucpr.cg;
+
+import br.pucpr.mage.*;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
+public class LitCube implements Scene {
+    private Keyboard keys = Keyboard.getInstance();
+
+    private static final float SPEED = (float) Math.toRadians(180);
+    
+    private Mesh mesh;
+    private float angleX = 0.0f;
+    private float angleY = 0.5f;
+    private Camera camera = new Camera();
+    
+    @Override
+    public void init() {
+        glEnable(GL_DEPTH_TEST);
+        glEnable(GL_CULL_FACE);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        mesh = MeshFactory.createCube();
+        camera.getPosition().y = 1.0f;
+    }
+
+    @Override
+    public void update(float secs) {
+        if (keys.isPressed(GLFW_KEY_ESCAPE)) {
+            glfwSetWindowShouldClose(glfwGetCurrentContext(), true);
+            return;
+        }
+
+        if (keys.isDown(GLFW_KEY_A)) {
+            angleY += SPEED * secs;
+        } else if (keys.isDown(GLFW_KEY_D)) {
+            angleY -= SPEED * secs;
+        }
+        
+        if (keys.isDown(GLFW_KEY_W)) {
+            angleX += SPEED * secs;
+        } else if (keys.isDown(GLFW_KEY_S)) {
+            angleX -= SPEED * secs;
+        }
+    }
+
+    @Override
+    public void draw() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        Shader shader = mesh.getShader();
+        shader.bind()
+            //Camera
+            .setUniform("uProjection", camera.getProjectionMatrix())
+            .setUniform("uView", camera.getViewMatrix())
+            .setUniform("uCameraPos", camera.getPosition())
+
+            //Luz
+            .setUniform("uLightDir", new Vector3f(1.0f, -1.0f, -1.0f))
+            .setUniform("uAmbientLight", new Vector3f(0.1f, 0.1f, 0.1f))   //Penumbra
+            .setUniform("uDiffuseLight", new Vector3f(1.0f, 1.0f, 0.8f))   //Luz amarelada
+            .setUniform("uSpecularLight", new Vector3f(1.0f, 1.0f, 1.0f))   //Luz amarelada
+
+            //Material
+            .setUniform("uAmbientMaterial", new Vector3f(0.5f, 0.0f, 0.5f))
+            .setUniform("uDiffuseMaterial", new Vector3f(0.5f, 0.0f, 0.5f))
+            .setUniform("uSpecularMaterial", new Vector3f(1.0f, 1.0f, 1.0f))
+            .setUniform("uSpecularPower", 32.0f)
+        .unbind();
+
+        mesh.setUniform("uWorld", new Matrix4f().rotateY(angleY).rotateX(angleX));
+        mesh.draw();
+    }
+
+    @Override
+    public void deinit() {
+    }
+
+    public static void main(String[] args) {
+        new Window(new LitCube(), "Cube with lights", 800, 600).show();
+    }
+}
